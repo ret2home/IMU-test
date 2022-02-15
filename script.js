46c1167fb67c78eb47e9ab8bc4660a94585d11ef
define <defineprogram@gmail.com>
2022-02-16 05:12:51 +0900

    a

--- script.js
+++ script.js
@@ -21,12 +21,14 @@ function updateFieldIfNotNull(fieldName,value){
     }
 }
 
-var speed_x=0;
-var speed_y=0;
-var locat_x=0;
-var locat_y=0;
-var timer=0;
-var last_timer=0;
+var speed_x=0,speed_y=0;
+var locat_x=0,locat_y=0;
+var timer=0,last_timer=0;
+var lowpass_x=0,highpass_x=0;
+var lowpass_y=0,highpass_y=0;
+var filter=0.9;
+var old_ac_x=0,old_ac_y=0;
+var old_speed_x=0,old_speed_y=0;
 
 function clock(){
     timer++;
@@ -37,12 +39,21 @@ function handleMotion(event){
     var ac_x=event.acceleration.x;
     var ac_y=event.acceleration.y;
     var int=(timer-last_timer)/1000;
-    var new_speed_x=speed_x+ac_x*int;
-    var new_speed_y=speed_y+ac_y*int;
-    locat_x+=(speed_x+new_speed_x)*int/2;
-    locat_y+=(speed_y+new_speed_y)*int/2;
-    speed_x=new_speed_x;
-    speed_y=new_speed_y;
+
+    lowpass_x=lowpass_x*filter+ac_x*(1-filter);
+    highpass_x=ac_x-lowpass_x;
+    speed_x=((highpass_x+old_ac_x)*int)/2+speed_x;
+    old_ac_x=highpass_x;
+    locat_x=((speed_x+old)*int)/2+locat_x;
+    old_speed_x=speed_x;
+
+    lowpass_y=lowpass_y*filter+ac_y*(1-filter);
+    highpass_y=ac_y-lowpass_y;
+    speed_y=((highpass_y+old_ac_y)*int)/2+speed_y;
+    old_ac_y=highpass_y;
+    locat_y=((speed_y+old)*int)/2+locat_y;
+    old_speed_y=speed_y;
+
     updateFieldIfNotNull("accel_x",ac_x);
     updateFieldIfNotNull("accel_y",ac_y);
     updateFieldIfNotNull("speed_x",speed_x);
