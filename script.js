6468a02017734b7597dcd5260d356f47be0df65a
define <defineprogram@gmail.com>
2022-02-16 04:43:30 +0900

    a

--- script.js
+++ script.js
@@ -22,8 +22,23 @@ function updateFieldIfNotNull(fieldName,value){
 
 var speed_x=0;
 var speed_y=0;
+var locat_x=0;
+var locat_y=0;
 function handleMotion(event){
-    updateFieldIfNotNull("accel_x",event.acceleration.x);
-    updateFieldIfNotNull("accel_y",event.acceleration.y);
-    updateFieldIfNotNull("interval",event.interval);
+    var ac_x=event.acceleration.x;
+    var ac_y=event.acceleration.y;
+    var int=event.interval;
+    var new_speed_x=speed_x+ac_x*int;
+    var new_speed_y=speed_y+ac_y*int;
+    locat_x+=(speed_x+new_speed_x)*int/2;
+    locat_y+=(speed_y+new_speed_y)*int/2;
+    speed_x=new_speed_x;
+    speed_y=new_speed_y;
+    updateFieldIfNotNull("accel_x",ac_x);
+    updateFieldIfNotNull("accel_y",ac_y);
+    updateFieldIfNotNull("speed_x",speed_x);
+    updateFieldIfNotNull("speed_y",speed_y);
+    updateFieldIfNotNull("locat_x",locat_x);
+    updateFieldIfNotNull("locat_y",locat_y);
+    updateFieldIfNotNull("interval",int);
 }
