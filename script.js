e2a55d3fc536aacb2f89972f0b2fe09e581705a4
define <defineprogram@gmail.com>
2022-02-16 04:51:21 +0900

    a

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ var locat_y=0;
 function handleMotion(event){
     var ac_x=event.acceleration.x;
     var ac_y=event.acceleration.y;
-    var int=event.interval;
+    var int=event.interval/1000;
     var new_speed_x=speed_x+ac_x*int;
     var new_speed_y=speed_y+ac_y*int;
     locat_x+=(speed_x+new_speed_x)*int/2;
