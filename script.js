0209a4c490184397f717bca50441d8a9b6bf57ba
Seiji Hirata <47498872+ret2home@users.noreply.github.com>
2022-02-18 08:11:28 +0900

    Update script.js

--- script.js
+++ script.js
@@ -209,7 +209,7 @@ function draw(){
             ctx.arc(400,400,100*i,0,2*Math.PI);
             ctx.stroke();
             ctx.font="24px serif";
-            ctx.fillText((String)(i*space),350+100*i,400);
+            ctx.fillText((String)(i*space),360+100*i,400);
         }
         ctx.beginPath();
         ctx.lineWidth="2";
