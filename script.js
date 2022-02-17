5e035af7f8573de58dc909d9104f6c1e5098d2ab
Seiji Hirata <47498872+ret2home@users.noreply.github.com>
2022-02-18 08:07:32 +0900

    Update script.js

--- script.js
+++ script.js
@@ -208,8 +208,8 @@ function draw(){
             ctx.strokeStyle="Black";
             ctx.arc(400,400,100*i,0,2*Math.PI);
             ctx.stroke();
-            ctx.font="12px serif";
-            ctx.fillText((String)(i*space),380+100*i,400);
+            ctx.font="16px serif";
+            ctx.fillText((String)(i*space),370+100*i,400);
         }
         ctx.beginPath();
         ctx.lineWidth="2";
