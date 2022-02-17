0eed457c4a2bd7dd467c72acbd8278015dd2e874
define <defineprogram@gmail.com>
2022-02-17 21:11:52 +0900

    a

--- script.js
+++ script.js
@@ -29,64 +29,71 @@ function updateFieldIfNotNull(fieldName,value){
         document.getElementById(fieldName).innerHTML=res;
     }
 }
-
-var speed_x=0,speed_y=0;
-var locat_x=0,locat_y=0;
-var timer=0,last_timer=0;
-var lowpass_x=0,highpass_x=0;
-var lowpass_y=0,highpass_y=0;
-var filter=0.9;
-var old_ac_x=0,old_ac_y=0;
-var old_speed_x=0,old_speed_y=0;
+var timer=0;
 function clock(){
     timer++;
 }
 setInterval(clock,10);
 
-function filterUp(){
-    filter+=0.05;
-}
-function filterDown(){
-    filter-=0.05;
-}
+var accel_x=[],accel_y=[],accel_z=[],tim=[];
 function handleMotion(event){
     var ac_x=event.acceleration.x;
     var ac_y=event.acceleration.y;
-    var int=(timer-last_timer)/100;
+    var ac_z=event.acceleration.z;
 
-
-    lowpass_x=lowpass_x*filter+ac_x*(1-filter);
-    highpass_x=ac_x-lowpass_x;
-    speed_x=((highpass_x+old_ac_x)*int)/2+speed_x;
-    old_ac_x=highpass_x;
-    locat_x=((speed_x+old_speed_x)*int)/2+locat_x;
-    old_speed_x=speed_x;
-    
-    lowpass_y=lowpass_y*filter+ac_y*(1-filter);
-    highpass_y=ac_y-lowpass_y;
-    speed_y=((highpass_y+old_ac_y)*int)/2+speed_y;
-    old_ac_y=highpass_y;
-    locat_y=((speed_y+old_speed_y)*int)/2+locat_y;
-    old_speed_y=speed_y;
-    
-
-    /*
-    var new_speed_x=speed_x+ac_x*int;
-    locat_x+=(speed_x+new_speed_x)*int/2;
-    speed_x=new_speed_x;
-    
-    var new_speed_y=speed_y+ac_y*int;
-    locat_y+=(speed_y+new_speed_y)*int/2;
-    speed_y=new_speed_y;
-    */
-
-    updateFieldIfNotNull("filter",filter);
+    accel_x.push(ac_x);
+    accel_y.push(ac_y);
+    accel_z.push(ac_z);
     updateFieldIfNotNull("accel_x",ac_x);
     updateFieldIfNotNull("accel_y",ac_y);
-    updateFieldIfNotNull("speed_x",speed_x);
-    updateFieldIfNotNull("speed_y",speed_y);
-    updateFieldIfNotNull("locat_x",locat_x);
-    updateFieldIfNotNull("locat_y",locat_y);
-    updateFieldIfNotNull("interval",int);
-    last_timer=timer;
+    updateFieldIfNotNull("accel_z",ac_z);
 }
+
+
+function draw(){
+    var canvas=document.getElementById("canvas");
+    if(canvas.getContext){
+        var ctx=canvas.getContext('2d');
+        ctx.clearRect(0,0,800,400);
+        ctx.beginPath();
+        ctx.lineWidth="2";
+        ctx.strokeStyle="Black";
+        ctx.moveTo(0,200);
+        ctx.lineTo(1000,200);
+        ctx.stroke();
+
+
+        ctx.beginPath();
+        ctx.lineWidth="0.5";
+        ctx.strokeStyle="Black";
+        ctx.moveTo(0,100);
+        ctx.lineTo(1000,100);
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.lineWidth="0.5";
+        ctx.strokeStyle="Black";
+        ctx.moveTo(0,300);
+        ctx.lineTo(1000,300);
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.lineWidth="1";
+        ctx.strokeStyle="Red";
+        if(accel_x.length){
+            let lasy=0;
+            for(let i=accel_x.length-1;i>=0;i--){
+                let x=800-(tim[tim.length-1]-tim[i])/100*60;
+                if(x<0)break;
+                let y=-accel_x[i]*9.8+200;
+                if(i==accel_x.length-1){
+                    ctx.moveTo(x,y);
+                }else{
+                    ctx.lineTo(x,y);
+                }
+            }
+        }
+        ctx.stroke();
+    }
+}
+setInterval(draw,100);
